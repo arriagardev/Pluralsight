9e8a35def2e4d3fdd4e59c3142f3267e49afda43
gdljarri <juan.arriaga@flextronics.com>
2017-02-02 15:13:40 -0600

    CS Interfaces

---
+++ C# Interfaces/CSInterfacesMod2/Person/PersonRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class PersonRepository
+    {
+        public List<Person> GetPeople()
+        {
+            //Person[] people =
+            //{
+            //    new Person() {FirstName="Juan", LastName="Perez", Registration= DateTime.Parse("12/01/1990"), Rating=10 },
+            //    new Person() {FirstName="Luis", LastName="Lopez", Registration= DateTime.Parse("12/01/1985"), Rating=10 },
+            //    new Person() {FirstName="Carlos", LastName="Sanchez", Registration= DateTime.Parse("12/01/1994"), Rating=10 },
+            //    new Person() {FirstName="Francisco", LastName="Gomez", Registration= DateTime.Parse("12/01/1995"), Rating=10 },
+            //    new Person() {FirstName="Fernando", LastName="Salinas", Registration= DateTime.Parse("12/01/1991"), Rating=10 },
+            //    new Person() {FirstName="Ivan", LastName="Bonilla", Registration= DateTime.Parse("12/01/1985"), Rating=10 },
+            //    new Person() {FirstName="Jose", LastName="Marquez", Registration= DateTime.Parse("12/01/1989"), Rating=10 },
+            //    new Person() {FirstName="Mario", LastName="Patlan", Registration= DateTime.Parse("12/01/1990"), Rating=10 }
+            //};
+
+            List<Person> people = new List<Person>();
+            people.Add(new Person() { FirstName = "Juan", LastName = "Perez", Registration = DateTime.Parse("12/01/1990"), Rating = 10 });
+            people.Add(new Person() { FirstName = "Luis", LastName = "Lopez", Registration = DateTime.Parse("12/01/1985"), Rating = 10 });
+            people.Add(new Person() { FirstName = "Carlos", LastName = "Sanchez", Registration = DateTime.Parse("12/01/1994"), Rating = 10 });
+            people.Add(new Person() { FirstName = "Francisco", LastName = "Gomez", Registration = DateTime.Parse("12/01/1995"), Rating = 10 });
+            people.Add(new Person() { FirstName = "Fernando", LastName = "Salinas", Registration = DateTime.Parse("12/01/1991"), Rating = 10 });
+            people.Add(new Person() { FirstName = "Ivan", LastName = "Bonilla", Registration = DateTime.Parse("12/01/1985"), Rating = 10 });
+            people.Add(new Person() { FirstName = "Jose", LastName = "Marquez", Registration = DateTime.Parse("12/01/1989"), Rating = 10 });
+            people.Add(new Person() { FirstName = "Mario", LastName = "Patlan", Registration = DateTime.Parse("12/01/1990"), Rating = 10 });
+
+            return people;
+        }
+    }
+}
---
+++ C# Interfaces/CSInterfacesMod2/PersonUI/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using MahApps.Metro.Controls;
+using Person;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace PersonUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        PersonRepository peoplerep = new PersonRepository();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConcrete_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+
+            List<Person.Person> people;
+            people = peoplerep.GetPeople();
+            foreach (var person in people)
+                ltbPerson.Items.Add(person);
+        }
+
+        private void btnInterface_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+
+            IEnumerable people;
+            people = peoplerep.GetPeople();
+            foreach (var person in people)
+                ltbPerson.Items.Add(person);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+        }
+
+        private void ClearListBox()
+        {
+            ltbPerson.Items.Clear();
+        }
+    }
+}
